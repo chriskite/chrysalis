821a6ab899333cc322dae9afef6b94f7211896c1
Chris Kite <chris@chriskite.com>
2013-07-15 11:16:45 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chrysalis::Application.config.secret_token = '7b36f13bca74ef3ffc5ae5166284a271300c761d5138813a3f4c372e200876f465284ccbbe65113a3282748fb3d08b73ad16d19244cf2f4af83f7005b514f414'
