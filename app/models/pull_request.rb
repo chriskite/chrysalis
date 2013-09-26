02deabbd29357053cf3b58b1b0ad7eab78b3dc5c
Chris Kite <chris@chriskite.com>
2013-09-26 17:11:02 -0500

    fix build.sh output redirection to not bg the task

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -131,7 +131,7 @@ class PullRequest < ActiveRecord::Base
     env_vars = env_vars.map { |env,val| "CHRYSALIS_#{env}=#{val}" }.join(" ")
 
     Rails.logger.info "Building #{build_path}"
-    system("cd #{build_path} && #{env_vars} ./build/build.sh &> build.log")
+    system("cd #{build_path} && #{env_vars} ./build/build.sh 2>&1 > build.log")
     raise "Error running build.sh" if 0 != $?
   end
 
